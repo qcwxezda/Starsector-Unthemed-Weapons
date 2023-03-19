61348c3202d431c095e8070f210fb93d9e018454
qcwxezda <hy_pi_corism@live.com>
2023-03-19 02:35:00 -0400

    Add subfolders to weapons graphics, add helios mortar, add pummeler

---
+++ src/weaponexpansion/combat/scripts/MinigunEffect.java
@@ -0,0 +1,42 @@
+package weaponexpansion.combat.scripts;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.*;
+import weaponexpansion.util.HeatGlowRenderer;
+
+@SuppressWarnings("unused")
+public class MinigunEffect implements EveryFrameWeaponEffectPlugin, OnFireEffectPlugin, WeaponEffectPluginWithInit {
+
+    float bonusFireRate = 0f;
+    static final float maxBonusFireRate = 1f;
+    static final float timeToMaxBonus = 10f;
+    float timeSinceLastFired = 0f;
+    static final float timeUntilDecay = 0.15f;
+    static final float decayRate = 0.333f;
+    HeatGlowRenderer heatGlowRenderer;
+
+    @Override
+    public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+        timeSinceLastFired += amount;
+        if (timeSinceLastFired > timeUntilDecay) {
+            bonusFireRate = Math.max(0f, bonusFireRate - decayRate * amount);
+        }
+        heatGlowRenderer.setAlpha(bonusFireRate / maxBonusFireRate);
+    }
+
+    @Override
+    public void onFire(DamagingProjectileAPI proj, WeaponAPI weapon, CombatEngineAPI engine) {
+        float origCooldown = weapon.getCooldown();
+        bonusFireRate += maxBonusFireRate * Math.min(timeSinceLastFired, origCooldown) / timeToMaxBonus;
+        bonusFireRate = Math.min(bonusFireRate, maxBonusFireRate);
+        weapon.setRemainingCooldownTo(origCooldown / (1 + bonusFireRate));
+        weapon.setRefireDelay(origCooldown);
+        timeSinceLastFired = 0f;
+    }
+
+    @Override
+    public void init(WeaponAPI weapon) {
+        heatGlowRenderer = new HeatGlowRenderer(weapon);
+        Global.getCombatEngine().addLayeredRenderingPlugin(heatGlowRenderer);
+    }
+}
